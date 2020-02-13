aaa067c730612d71488e159d272f1a6b8b6a71ed
Isaiah Harrison <ijh5005@outlook.com>
2020-02-12 23:41:15 -0500

    finished

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080 || process.env.PORT;
 const axios = require("axios");
+const fakeData = require("./fakeData");
+const cors = require('cors');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
 
 let apiData = [];
 
@@ -16,11 +19,14 @@ const returnData = (res, data) => {
 }
 
 app.get("/", (req, res) => {
-    axios.get("https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition?patient=1316024&status=active")
-        .then(data => {
-            apiData = data.data;
-            res.json(data.data)
-        }).catch(err => res.send(err));
+    apiData = fakeData;
+    returnData(res, apiData.entry);
+
+    // axios.get("https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Condition?patient=1316024&status=active")
+    //     .then(data => {
+    //         apiData = data.data;
+    //         res.json(data.data)
+    //     }).catch(err => res.send(err));
 });
 
 const filter = (req, res) => {
@@ -47,12 +53,6 @@ const filter = (req, res) => {
     returnData(res, finalFilteredItems.entry)
 }
 
-app.get("/filter", (req, res) => filter(req, res));
+app.post("/filter", (req, res) => filter(req, res));
 
 app.listen(PORT, console.log(`server listening on port ${PORT}`));
-
-// filters
-// clinicalStatus: resolved, active
-// dateRecorded
-// verificationStatus: confirmed, differential
-// resource
